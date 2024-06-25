4896df81fe5b46a3ef194f1b038e04fa206d9b7a
Md Ryad Ahmed Biplob <rb147258@gmail.com>
2024-06-25 12:45:52 +0200

    updated to aladhan api

--- PrayerSchedule.py
+++ PrayerSchedule.py
@@ -1,28 +1,34 @@
 from typing import Literal
 import requests
+from datetime import date
 
-PRAYER = ('fajr', 'dhuhr', 'asr', 'maghrib', 'isha')
+PRAYER = ('Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha')
 
 class PrayerSchedule:
-    def __init__(self, api_key: str):
-        self.__api_key = api_key
+    def __init__(self, city: str, country: str):
+        self.__city = city
+        self.__country = country
     
-    def __get_24hr_time(self, time: str) -> tuple[int, int]:
-        hh_mm, am_pm = time.split(" ")
+    def __get_24hr_time(self, time: str) -> dict[Literal['hh', 'mm'], int]:
+        hh_mm, *am_pm = time.split(" ")
         hh, mm = hh_mm.split(":")
         hh, mm = int(hh), int(mm)
         
         if am_pm == "am":
             if hh == 12:
                 hh = 0
-        else:
+        elif am_pm == "pm":
             if hh != 12:
                 hh += 12
             
-        return (hh, mm)
+        return {'hh': hh, 'mm': mm}
 
-    def get_timings(self, location: str = "dortmund") -> dict[Literal['fajr', 'dhuhr', 'asr', 'maghrib', 'isha'], tuple[int, int]]:
-        response = requests.get(f"https://muslimsalat.com/{location}/daily.json", {'key': self.__api_key})
-        prayer_timings = response.json()['items'][0]
-
-        return { prayer: self.__get_24hr_time(prayer_timings[prayer]) for prayer in PRAYER }
+    def get_timings(self) -> dict[Literal['fajr', 'dhuhr', 'asr', 'maghrib', 'isha'], dict[Literal['hh', 'mm'], int]]:
+        today = date.today().strftime('%d-%m-%Y')
+        response = requests.get(f"http://api.aladhan.com/v1/timingsByCity/{today}", {'city': self.__city, 'country': self.__country, 'method': 3}).json()
+        
+        if response['code'] == 200:
+            prayer_timings = response['data']['timings']
+            return { prayer: self.__get_24hr_time(prayer_timings[prayer]) for prayer in PRAYER }
+        else:
+            raise Exception('Failed to pull new Prayer Schedule from API')    
--- scheduler.py
+++ scheduler.py
@@ -1,18 +1,35 @@
-from os import getcwd
+from os import getcwd, getenv
+from typing import Literal
+from PrayerSchedule import PrayerSchedule
 
 DEFAULT_PYTHON = f"{getcwd()}/.venv/bin/python"
+USER = getenv('USER')
+PYTHON = getenv('PYTHON', DEFAULT_PYTHON)
+CITY = getenv('CITY')
+COUNTRY = getenv('COUNTRY')
 
 def generate_cron_job_string(
         cron_schedule: str, 
         script_location: str,
-        flags: str, 
+        flags: str = "", 
         user: str = "ryad_pi", 
         python: str = DEFAULT_PYTHON
     ) -> str:
     return f"{cron_schedule} {user} {python} {script_location} {flags}"
 
-def init_cron_job():
-    pass
+def get_prayer_schedule_and_generate_cron_jobs():
+    prayer_schedule = PrayerSchedule(CITY, COUNTRY)
+    timings = prayer_schedule.get_timings()
+    for prayer, time in timings.items():
+        create_prayer_call_job(prayer, time)
+    
 
-def create_prayer_call_job():
-    pass
+def create_prayer_call_job(prayer: str, time: dict[Literal['hh', 'mm'], int]):
+    script_location = f"{getcwd()}/"
+    if prayer == 'Fajr':
+        print(generate_cron_job_string(f"{time['mm']} {time['hh']} * * *", script_location, "--fajr", user=USER, python=PYTHON))
+    else:
+        print(generate_cron_job_string(f"{time['mm']} {time['hh']} * * *", script_location, user=USER, python=PYTHON))
+
+if __name__ == '__main__':
+    get_prayer_schedule_and_generate_cron_jobs()
